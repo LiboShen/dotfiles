47078b420a4afd0269f9635529dfecab451ea071
Libo Shen <liboooshen@gmail.com>
2018-02-10 12:03:30 +0000

    Initial zhsrc.

---
+++ zsh/zshrc.zsh
@@ -0,0 +1,11 @@
+export EDITOR="nvim"
+export DOTFILES="$HOME/dotfiles"
+
+# Fast interation
+alias sz='source ~/.zshrc'
+alias ez='$EDITOR ~/.zshrc'
+alias ea='$EDITOR ~/dotfile/zsh/aliases.zsh'
+
+# Load zsh plugins
+source ~/.zsh_plugins.sh
+
