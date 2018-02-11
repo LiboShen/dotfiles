90c59409587937a74977cae790c6e316a068cf00
Libo Shen <liboooshen@gmail.com>
2018-02-11 18:39:57 +0000

    zshrc load aliases setting file

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -9,3 +9,14 @@ alias ea='$EDITOR ~/dotfile/zsh/aliases.zsh'
 # Load zsh plugins
 source ~/.zsh_plugins.sh
 
+# History
+HISTFILESIZE=8000
+HISTSIZE=80000
+HISTFILE=~/.zsh_history
+SAVEHIST=8000
+setopt  appendhistory
+setopt sharehistory
+setopt incappendhistory
+
+# Load aliases
+source "$DOTFILES/zsh/aliases.zsh"
