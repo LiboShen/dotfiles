807655bd72154b8f7e8f1022b2cafc1449ef5e0d
Libo <liboooshen@gmail.com>
2020-03-14 15:18:27 +0000

    Add git config.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
-ln -s ~/dotfiles/vim .configs/nvim
+ln -s ~/dotfiles/vim .config/nvim
 ln -s ~/dotfiles/zsh/zshrc.zsh .zshrc
+ln -s ~/dotfiles/git/gitconfig .gitconfig
+ln -s ~/dotfiles/git/gitigone_global .gitigone_global
 
