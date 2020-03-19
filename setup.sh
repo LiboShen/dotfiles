72e4f1c71901d090f352f9c9129300fced5a4729
Libo Shen <liboooshen@gmail.com>
2020-03-19 22:26:19 +0000

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-ln -s ~/dotfiles/vim .config/nvim
-ln -s ~/dotfiles/zsh/zshrc.zsh .zshrc
-ln -s ~/dotfiles/git/gitconfig .gitconfig
-ln -s ~/dotfiles/git/gitigone_global .gitigone_global
+ln -s ~/dotfiles/vim ~/.config/nvim
+ln -s ~/dotfiles/zsh/zshrc.zsh ~/.zshrc
+ln -s ~/dotfiles/git/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/git/gitigone_global ~/.gitigone_global
 
