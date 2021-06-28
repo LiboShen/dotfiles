77bcad78498e7d1c08354f20d033c6979a1f9781
Libo Shen <liboooshen@gmail.com>
2021-06-28 11:03:22 +0100

    update doom config

--- doom/config.el
+++ doom/config.el
@@ -106,15 +106,6 @@ same directory as the org-buffer and insert a link to this file."
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
-(setq
- projectile-project-root-files-functions '(projectile-root-top-down
-                                           ;projectile-root-top-down-recurring
-                                           )
- projectile-project-root-files
- '(".git" ".projectile" "mix.exs" "package.json")
- )
-
-
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
 ;; - `load!' for loading external *.el files relative to this one
@@ -145,12 +136,29 @@ same directory as the org-buffer and insert a link to this file."
   (push "[/\\\\]deps" lsp-file-watch-ignored )
   (push "[/\\\\].elixir_ls" lsp-file-watch-ignored )
   :hook
-  (elixir-mode . lsp)
+  ((elixir-mode . lsp)
+   (dhall-mode . lsp))
   )
+
+(map! :mode python-mode
+      :localleader
+      :desc "iMenu" :nv "c /" #'lsp-ui-imenu)
 
 (map! :mode elixir-mode
       :localleader
       :desc "iMenu" :nv "/" #'lsp-ui-imenu)
+
+(use-package! dhall-mode
+  :config
+  (setq
+   ;; uncomment the next line to disable automatic format
+   dhall-format-at-save nil
+
+   ;; comment the next line to use unicode syntax
+   dhall-format-arguments (\` ("--ascii"))
+
+   ;; header-line is obsoleted by lsp-mode
+   dhall-use-header-line nil))
 
 ;; To avoid annoying ocaml-ls's json warning messages.
 (setq warning-minimum-level :error)
@@ -167,6 +175,18 @@ same directory as the org-buffer and insert a link to this file."
 (setq-default abbrev-mode t)
 
 (sp-local-pair 'reason-mode "`" nil :actions nil) ;; disable this smartparens for reason mode.
+
+(use-package! projectile
+  :config
+  (setq
+   projectile-project-root-files-functions '(projectile-root-top-down
+                                        ;projectile-root-top-down-recurring
+                                             ))
+  (setq
+   projectile-project-root-files
+   '(".git" ".projectile" "mix.exs" "package.json" "pyproject.toml"))
+  )
+
 
 (use-package! slack
   :commands (slack-start)
@@ -228,107 +248,11 @@ same directory as the org-buffer and insert a link to this file."
 
 (setq +ivy-buffer-preview t)
 
-;; ;; Better org-return.
-;; ;; From https://github.com/alphapapa/unpackaged.el#org-return-dwim
-
-;; (defun unpackaged/org-element-descendant-of (type element)
-;;   "Return non-nil if ELEMENT is a descendant of TYPE.
-;; TYPE should be an element type, like `item' or `paragraph'.
-;; ELEMENT should be a list like that returned by `org-element-context'."
-;;   ;; MAYBE: Use `org-element-lineage'.
-;;   (when-let* ((parent (org-element-property :parent element)))
-;;     (or (eq type (car parent))
-;;         (unpackaged/org-element-descendant-of type parent))))
-
-;; ;;;###autoload
-;; (defun unpackaged/org-return-dwim (&optional default)
-;;   "A helpful replacement for `org-return-indent'.  With prefix, call `org-return-indent'.
-
-;; On headings, move point to position after entry content.  In
-;; lists, insert a new item or end the list, with checkbox if
-;; appropriate.  In tables, insert a new row or end the table."
-;;   ;; Inspired by John Kitchin: http://kitchingroup.cheme.cmu.edu/blog/2017/04/09/A-better-return-in-org-mode/
-;;   (interactive "P")
-;;   (if default
-;;       (org-return t)
-;;     (cond
-;;      ;; Act depending on context around point.
-
-;;      ;; NOTE: I prefer RET to not follow links, but by uncommenting this block, links will be
-;;      ;; followed.
-
-;;      ;; ((eq 'link (car (org-element-context)))
-;;      ;;  ;; Link: Open it.
-;;      ;;  (org-open-at-point-global))
-
-;;      ((org-at-heading-p)
-;;       ;; Heading: Move to position after entry content.
-;;       ;; NOTE: This is probably the most interesting feature of this function.
-;;       (let ((heading-start (org-entry-beginning-position)))
-;;         (goto-char (org-entry-end-position))
-;;         (cond ((and (org-at-heading-p)
-;;                     (= heading-start (org-entry-beginning-position)))
-;;                ;; Entry ends on its heading; add newline after
-;;                (end-of-line)
-;;                (insert "\n\n"))
-;;               (t
-;;                ;; Entry ends after its heading; back up
-;;                (forward-line -1)
-;;                (end-of-line)
-;;                (when (org-at-heading-p)
-;;                  ;; At the same heading
-;;                  (forward-line)
-;;                  (insert "\n")
-;;                  (forward-line -1))
-;;                ;; FIXME: looking-back is supposed to be called with more arguments.
-;;                (while (not (looking-back (rx (repeat 3 (seq (optional blank) "\n")))))
-;;                  (insert "\n"))
-;;                (forward-line -1)))))
-
-;;      ((org-at-item-checkbox-p)
-;;       ;; Checkbox: Insert new item with checkbox.
-;;       (org-insert-todo-heading nil))
-
-;;      ((org-in-item-p)
-;;       ;; Plain list.  Yes, this gets a little complicated...
-;;       (let ((context (org-element-context)))
-;;         (if (or (eq 'plain-list (car context))  ; First item in list
-;;                 (and (eq 'item (car context))
-;;                      (not (eq (org-element-property :contents-begin context)
-;;                               (org-element-property :contents-end context))))
-;;                 (unpackaged/org-element-descendant-of 'item context))  ; Element in list item, e.g. a link
-;;             ;; Non-empty item: Add new item.
-;;             (org-insert-item)
-;;           ;; Empty item: Close the list.
-;;           ;; TODO: Do this with org functions rather than operating on the text. Can't seem to find the right function.
-;;           (delete-region (line-beginning-position) (line-end-position))
-;;           (insert "\n"))))
-
-;;      ((when (fboundp 'org-inlinetask-in-task-p)
-;;         (org-inlinetask-in-task-p))
-;;       ;; Inline task: Don't insert a new heading.
-;;       (org-return t))
-
-;;      ((org-at-table-p)
-;;       (cond ((save-excursion
-;;                (beginning-of-line)
-;;                ;; See `org-table-next-field'.
-;;                (cl-loop with end = (line-end-position)
-;;                         for cell = (org-element-table-cell-parser)
-;;                         always (equal (org-element-property :contents-begin cell)
-;;                                       (org-element-property :contents-end cell))
-;;                         while (re-search-forward "|" end t)))
-;;              ;; Empty row: end the table.
-;;              (delete-region (line-beginning-position) (line-end-position))
-;;              (org-return t))
-;;             (t
-;;              ;; Non-empty row: call `org-return-indent'.
-;;              (org-return t))))
-;;      (t
-;;       ;; All other cases: call `org-return-indent'.
-;;       (org-return t)))))
-
-;; (map!
-;;  :after evil-org
-;;  :map evil-org-mode-map
-;;  :i [return] #'unpackaged/org-return-dwim)
+(use-package! edit-server
+  :if window-system
+  :defer 2
+  :config
+  (edit-server-start)
+  :hook
+  (edit-server-start-hook . markdown-mode)
+  )
